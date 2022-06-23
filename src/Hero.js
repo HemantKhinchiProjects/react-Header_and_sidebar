b8593f752781fff31cc5c94117b71dbda3fbdd9c
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-06-23 08:38:46 +0530

    1 file modified

--- src/Hero.js
+++ src/Hero.js
@@ -2,9 +2,20 @@ import React from 'react';
 import phoneImg from './images/phone.svg';
 import { useGlobalContext } from './context';
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
-  return <h2>hero component</h2>;
+  const {closeSubmenu} = useGlobalContext();
+  
+  return <section className="hero">
+    <div className="hero-center">
+      <article className="hero-info">
+        <h1>Payment infrastructure for the internet</h1>
+        <p>Millions of companys of all size-from startups to fortune 500s-use Stripe's  software and API's to accept payments, send payout, and manage their busniess online.</p>
+        <button className="btn">Start now</button>
+      </article>
+      <article className="hero-images">
+        <div className="phone-img"></div>
+      </article>
+      </div>
+  </section>;
 };
 
 export default Hero;
